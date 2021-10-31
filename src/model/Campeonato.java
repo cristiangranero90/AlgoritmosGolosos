71636449edf3789f1f335431c4bd4bd49ed01e3e
Cristian Gabriel <cristian_granero90@yahoo.com>
2021-10-31 20:56:18 -0300

    Se agrego interactividad en campeonato

--- src/model/Campeonato.java
+++ src/model/Campeonato.java
@@ -13,14 +13,41 @@ public class Campeonato {
 	}
 
 	
-	@SuppressWarnings("unchecked")
 	public ArrayList<Fecha> getFechas() {
 		if (this.fechas != null) {
-			return (ArrayList<Fecha>) fechas.clone();
+			return (ArrayList<Fecha>) fechas;
 		}
 		else {
 			throw new RuntimeException("No existen fechas o no hay fechas cargadas. ");
 		}		
+	}
+	
+	public boolean agregarFechas(Fecha nuevaFecha) {
+		return getFechas().add(nuevaFecha);
+	}
+	
+	public boolean existeFecha(Fecha otraFecha) {
+		return getFechas().contains(otraFecha);
+	}
+	
+	public ArrayList<Arbitro> getArbitrosDisponibles() {
+		if (this.arbitrosDisponibles != null) {
+			return (ArrayList<Arbitro>) arbitrosDisponibles;
+		}
+		else {
+			throw new RuntimeException("No existen arbitros o no hay arbitros cargados. ");
+		}		
+	}
+	
+	public boolean agregarArbitro(Arbitro nuevo) {
+		if (!existeArbitro(nuevo)) {
+			return getArbitrosDisponibles().add(nuevo);
+		}
+		return false;
+	}
+
+	private boolean existeArbitro(Arbitro nuevo) {		
+		return arbitrosDisponibles.contains(nuevo);
 	}
 	
 }
