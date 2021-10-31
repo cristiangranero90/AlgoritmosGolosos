b3f1fcff6fb649515402d6d5e977031f07cd4d26
Lorenzo1990 <vttloort@gmail.com>
2021-10-31 18:48:30 -0300

    se agrega libreria de JSON y el archivo.

--- src/persistencia/PartidosPersistentes.java
+++ src/persistencia/PartidosPersistentes.java
@@ -1,6 +1,41 @@
 package persistencia;
 
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
+
+import org.json.simple.JSONObject;
+
 public class PartidosPersistentes {
 
 	//Para GSON JSON
+	public void lectura (FileReader file)
+	{
+		JSONParser parser = new JSONParser();
+
+		try {
+			Object obj= parser.parse(new FileReader("prueba.json"));
+			
+			JSONObject jsonobj= (JSONObject) obj;
+			
+			System.out.println("JSON leido"+ jsonobj);
+			}
+
+		 catch (FileNotFoundException e) {
+			//manejo de error
+		} catch (IOException e) {
+			//manejo de error
+		} catch (ParseException e) {
+			System.out.println("se lanzó esta exception");
+		}
+
+	}
+	
+	
 }
