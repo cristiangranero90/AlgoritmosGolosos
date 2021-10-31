a4e82370d97c39123a982056d5fe5c57099d4314
Cristian Gabriel <cristian_granero90@yahoo.com>
2021-10-31 20:48:21 -0300

    Los arbitros son de un campeonato en particlar

--- src/model/Campeonato.java
+++ src/model/Campeonato.java
@@ -5,13 +5,22 @@ import java.util.ArrayList;
 public class Campeonato {
 	
 	private ArrayList<Fecha> fechas;
+	private ArrayList<Arbitro> arbitrosDisponibles;
 
 	public Campeonato() {
 		fechas = new ArrayList<>();
+		arbitrosDisponibles = new ArrayList<>();
 	}
 
+	
+	@SuppressWarnings("unchecked")
 	public ArrayList<Fecha> getFechas() {
-		return fechas;
+		if (this.fechas != null) {
+			return (ArrayList<Fecha>) fechas.clone();
+		}
+		else {
+			throw new RuntimeException("No existen fechas o no hay fechas cargadas. ");
+		}		
 	}
 	
 }
