794578eea40fc6faa81d383835fa6bac332527ac
Cristian Gabriel <cristian_granero90@yahoo.com>
2021-11-01 10:55:45 -0300

    Cambios en el constructor de partido

--- src/model/Partido.java
+++ src/model/Partido.java
@@ -5,20 +5,35 @@ public class Partido {
 	private Club[] encuentro;
 	private Arbitro arbitro;
 	
-	public Partido(Arbitro arbitroSeleccionado) {
+	public Partido(Club uno, Club dos) {
 		encuentro = new Club[1];
-		arbitro = arbitroSeleccionado;
+		encuentro[0] = uno;
+		encuentro[1] = dos;
+	}
+	
+	public Club getClub(int indice) throws Exception {
+		if (indice == 0 || indice == 1) {
+			return getEncuentro()[indice];
+		}
+		else {
+			throw new RuntimeException("El indice ingresado para club, es invalido. ");
+		}
 	}
 
 	public Club[] getEncuentro() {
-		return encuentro;
+		return this.encuentro;
 	}
-
-	public void setEncuentro(Club[] encuentro) {
-		this.encuentro = encuentro;
+	
+	public void setArbitro(Arbitro nuevo) {
+		if (arbitro != null) {
+			this.arbitro = nuevo;
+		}
+		else {
+			throw new RuntimeException("El arbitro ingresado es invalido. ");
+		}
 	}
 
 	public Arbitro getArbitro() {
-		return arbitro;
+		return this.arbitro;
 	}	
 }
