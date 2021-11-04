1f5eb1144866d1f87f68d3eb8ea58c180edb8059
Lorenzo1990 <vttloort@gmail.com>
2021-11-04 01:23:32 -0300

    view

--- src/view/CampeonatoWindow.java
+++ src/view/CampeonatoWindow.java
@@ -3,10 +3,20 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JList;
+import java.awt.BorderLayout;
+import javax.swing.JComboBox;
+import java.awt.Color;
+import java.awt.GridLayout;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import javax.swing.JLabel;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
 
 public class CampeonatoWindow {
 
-	private JFrame frame;
+	private JFrame frmTp;
 
 	/**
 	 * Launch the application.
@@ -16,7 +26,7 @@ public class CampeonatoWindow {
 			public void run() {
 				try {
 					CampeonatoWindow window = new CampeonatoWindow();
-					window.frame.setVisible(true);
+					window.frmTp.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +45,25 @@ public class CampeonatoWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTp = new JFrame();
+		frmTp.setIconImage(Toolkit.getDefaultToolkit().getImage(CampeonatoWindow.class.getResource("/view/images.jpg")));
+		frmTp.setTitle("TP 3 - ALGORITMO GOLOSO");
+		frmTp.setBounds(100, 100, 533, 396);
+		frmTp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTp.getContentPane().setLayout(null);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(10, 46, 79, 22);
+		frmTp.getContentPane().add(comboBox);
+		comboBox.addItem(1);comboBox.addItem(2);comboBox.addItem(3);
+		comboBox.addItem(4);comboBox.addItem(5);comboBox.addItem(6);
+		comboBox.addItem(7);comboBox.addItem(8);comboBox.addItem(9);
+JLabel lblNewLabel = new JLabel("Seleccionar fecha");
+lblNewLabel.setBounds(10, 21, 111, 14);
+frmTp.getContentPane().add(lblNewLabel);
+JLabel lblNewLabel_1 = new JLabel("");
+lblNewLabel_1.setIcon(new ImageIcon(CampeonatoWindow.class.getResource("/view/images.jpg")));
+lblNewLabel_1.setBounds(131, 0, 386, 357);
+frmTp.getContentPane().add(lblNewLabel_1);
 	}
-
 }
