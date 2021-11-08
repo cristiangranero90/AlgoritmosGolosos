fa51c210c1643762fa35ad3d2546c4710ac99005
Cristian Gabriel <cristian_granero90@yahoo.com>
2021-11-08 10:59:09 -0300

    Warnings deleted

--- src/model/ordenarPorAparicion.java
+++ src/model/ordenarPorAparicion.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.Comparator;
-
 public class ordenarPorAparicion extends Ordenar{
 
 	@Override
--- src/model/ordenarPorCantApar.java
+++ src/model/ordenarPorCantApar.java
@@ -1,7 +1,5 @@
 package model;
 
-import java.util.Comparator;
-
 public class ordenarPorCantApar extends Ordenar{
 
 	@Override
